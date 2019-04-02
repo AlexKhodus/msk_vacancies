98ecfc4c1b623ccb93e9ed5e0be20087ad4f91a0
Blaze-Front-end <44093005+Blaze-Front-end@users.noreply.github.com>
2019-04-02 11:23:42 +0300

    add to js task babel and sourcemap

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,8 +1,13 @@
 module.exports = ($, path) => {
     $.gulp.task('js', () => {
         return $.gulp.src(path.src.js)
+            .pipe($.sourcemaps.init())
             .pipe($.concat('scripts.min.js'))
+            .pipe($.babel({
+                presets: ['@babel/preset-env']
+            }))
             .pipe($.uglify())
+            .pipe($.sourcemaps.write('.'))
             .pipe($.gulp.dest(path.build.js))
     });
 };
